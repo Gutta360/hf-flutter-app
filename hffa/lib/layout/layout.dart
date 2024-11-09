a555b2bfa964fd2c4d8898fa5359aaaee7dc49d4
gutta <gutta.ramakoteswararao@gmail.com>
2024-11-09 17:42:52 +0530

    logout functionality

--- hffa/lib/layout/layout.dart
+++ hffa/lib/layout/layout.dart
@@ -4,8 +4,21 @@ import 'package:hffa/layout/register.dart';
 import 'package:hffa/layout/transaction.dart';
 import 'package:hffa/layout/calculator.dart';
 
-class LayoutWidget extends StatelessWidget {
+class LayoutWidget extends StatefulWidget {
   const LayoutWidget({super.key});
+
+  @override
+  _LayoutWidgetState createState() => _LayoutWidgetState();
+}
+
+class _LayoutWidgetState extends State<LayoutWidget> {
+  bool isLoggedIn = false;
+
+  void handleLoginSuccess() {
+    setState(() {
+      isLoggedIn = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,39 +26,56 @@ class LayoutWidget extends StatelessWidget {
       length: 4,
       child: Scaffold(
         appBar: AppBar(
-            bottom: const TabBar(
-              indicatorColor: Colors.white, // Tab indicator color
-              labelColor: Colors.black, // Selected icon color
-              unselectedLabelColor: Colors.white, // Unselected icon color
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.login),
-                  text: 'Login',
-                ),
-                Tab(
-                  icon: Icon(Icons.app_registration_rounded),
-                  text: 'Register',
-                ),
-                Tab(
-                  icon: Icon(Icons.article_outlined),
-                  text: 'Transaction',
-                ),
-                Tab(
-                  icon: Icon(Icons.calculate),
-                  text: 'Calculator',
-                )
-              ],
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.white,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.login),
+                text: 'Login',
+              ),
+              Tab(
+                icon: Icon(Icons.app_registration_rounded),
+                text: 'Register',
+              ),
+              Tab(
+                icon: Icon(Icons.article_outlined),
+                text: 'Transaction',
+              ),
+              Tab(
+                icon: Icon(Icons.calculate),
+                text: 'Calculator',
+              ),
+            ],
+          ),
+          title: const Text(
+            'Harini Fertilizers',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
-            title: const Text('Harini Fertilizers',
-                style: TextStyle(
-                    fontSize: 24, // Sets the font size
-                    fontWeight: FontWeight.bold)), // Makes the text italic
-
-            centerTitle: true,
-            backgroundColor: Colors.teal),
-        body: TabBarView(
-          children: [LoginPage(), RegisterForm(), TxnForm(), TxnSearchForm()],
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.teal,
         ),
+        body: TabBarView(
+          children: [
+            LoginPage(onLoginSuccess: handleLoginSuccess), // Pass callback
+            isLoggedIn ? RegisterForm() : _buildDisabledTab(),
+            isLoggedIn ? TxnForm() : _buildDisabledTab(),
+            isLoggedIn ? TxnSearchForm() : _buildDisabledTab(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDisabledTab() {
+    return Center(
+      child: Text(
+        'Please log in to access this tab.',
+        style: TextStyle(fontSize: 18, color: Colors.grey),
       ),
     );
   }
--- hffa/lib/layout/login.dart
+++ hffa/lib/layout/login.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
+  final VoidCallback onLoginSuccess;
+
+  LoginPage({required this.onLoginSuccess});
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -9,106 +13,137 @@ class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   String _email = '';
   String _password = '';
+  bool isLoggedIn = false; // Track login state
 
-  // Controllers to hold the email and password values
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  // Login logic
   void _login() {
     if (_formKey.currentState!.validate()) {
-      // Perform login logic here
-      // Example: print email and password
+      // Simulate login logic
       print('Logging in with $_email and $_password');
+
+      setState(() {
+        isLoggedIn = true;
+      });
+
+      // Notify the parent widget about the successful login
+      widget.onLoginSuccess();
     }
   }
 
-  // Forgot password logic
-  void _forgotPassword() {
-    // Add forgot password logic
-    print('Redirect to forgot password screen');
+  void _logout() {
+    setState(() {
+      isLoggedIn = false;
+      _emailController.clear();
+      _passwordController.clear();
+    });
+    print('User logged out');
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Center(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 400),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Email TextField
-                  TextFormField(
-                    controller: _emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        labelText: 'Email', prefixIcon: Icon(Icons.email)),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter an email';
-                      } else if (!RegExp(
-                              r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
-                          .hasMatch(value)) {
-                        return 'Please enter a valid email';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      _email = value;
-                    },
-                  ),
-                  SizedBox(height: 16),
-                  // Password TextField
-                  TextFormField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      prefixIcon: Icon(Icons.lock),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 400),
+          child: isLoggedIn
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'You logged in Successfully!!!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a password';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      _password = value;
-                    },
-                  ),
-                  SizedBox(height: 38),
-                  // Login Button
-                  ElevatedButton(
-                    onPressed: _login,
-                    child: Text('Login'),
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.teal, // Text color
-                      padding: EdgeInsets.symmetric(vertical: 14),
-                      minimumSize: Size(120, 40),
-                      textStyle: TextStyle(
-                        fontSize: 15, // Font size // Optional: makes text bold
-                      ), // Reduced height and width
+                    SizedBox(height: 38),
+                    ElevatedButton(
+                      onPressed: _logout,
+                      child: Text('Logout'),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.teal,
+                        padding: EdgeInsets.symmetric(vertical: 14),
+                        minimumSize: Size(120, 40),
+                        textStyle: TextStyle(fontSize: 15),
+                      ),
                     ),
+                  ],
+                )
+              : Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          prefixIcon: Icon(Icons.email),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter an email';
+                          } else if (!RegExp(
+                                  r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+                              .hasMatch(value)) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          _email = value;
+                        },
+                      ),
+                      SizedBox(height: 16),
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          prefixIcon: Icon(Icons.lock),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a password';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          _password = value;
+                        },
+                      ),
+                      SizedBox(height: 38),
+                      ElevatedButton(
+                        onPressed: _login,
+                        child: Text('Login'),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.teal,
+                          padding: EdgeInsets.symmetric(vertical: 14),
+                          minimumSize: Size(120, 40),
+                          textStyle: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextButton(
+                        onPressed: _forgotPassword,
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 16),
-                  // Forgot Password Link
-                  TextButton(
-                    onPressed: _forgotPassword,
-                    child: Text('Forgot Password?',
-                        style: TextStyle(color: Colors.blue)),
-                  ),
-                ],
-              ),
-            ),
-          ),
+                ),
         ),
       ),
     );
+  }
+
+  void _forgotPassword() {
+    print('Redirect to forgot password screen');
   }
 }
